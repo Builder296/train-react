c024fd827fd16e15e597daec4ce83c0b467c895c
builder <builder@odds.team>
2020-08-15 14:25:06 +0700

    Quiz

--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ class ItemEvent extends React.Component {
   }
 
   componentDidMount() {
-    // setTimeout(() => {
-    //   const state = this.state.numbers;
-    //   state.shift();
-    //   this.setState({ numbers: [...state]});
-    // })
+    setTimeout(() => {
+      const state = this.state.numbers;
+      state.shift();
+      this.setState({ numbers: [...state]});
+    })
   }
 
   add = () => {
@@ -39,9 +39,35 @@ function ItemList({numbers}) {
   </>
 }
 
+class NewItemEventQuiz extends React.Component {
+
+  state = {
+    listText: [],
+    text: ""
+  }
+
+  add = () => {
+    this.setState({ listText: [...this.state.listText, this.state.text]})
+  }
+
+  onChange(event) {
+    this.setState({ text: event.target.value});
+  }
+
+  render() {
+    const { listText } = this.state;
+    return <>
+      <input onChange={(event) => this.onChange(event)} />
+      <button onClick={this.add}>Add</button>
+      <ItemList numbers = {listText}/>
+    </>
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <ItemEvent />
+    <NewItemEventQuiz />
   </React.StrictMode>,
   document.getElementById('root')
 );
