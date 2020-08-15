6d11eb35e5af897b6fe05544be6bb641e131c571
builder <builder@odds.team>
2020-08-15 12:07:15 +0700

    Quiz

--- src/index.js
+++ src/index.js
@@ -5,60 +5,44 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 // condition rendering
-class HelloWorldComp extends React.Component {
+class Quiz extends React.Component {
 
   constructor(props) {
     super(props) // กรุณาส่ง props มาให้ด้วย
     this.state = {
-      message: "My first state",
       count: 0,
-      countMore: 2,
     };
   }
 
+  isOddOrEven(number) {
+    if (number % 2 === 0) {
+      return "even"
+    }
+    return "odd"
+  }
+
   onAdd = () => {
-    const { countMore } = this.state;
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-      countMore: countMore + 2
-    }));
+    this.setState({ count: this.state.count + 1 })
   }
 
   render() {
-    if(this.state.count % 3 === 2) {
-      return <h2>This is a book.</h2>
+    const { count } = this.state;
+    let items = [];
+    for (let i = 1 ; i <= count ; i++  ) {
+      items.push(<li key={i}> {i} : {this.isOddOrEven(i)} </li>)
     }
-    const {message, count, countMore} = this.state 
     return <>
-      <h1> { message } :: { count } : { countMore }</h1>
       <button onClick={this.onAdd}>Add</button>
+      <ol>
+        { items }
+      </ol>
     </>
   }
 }
 
-function Car({isHonda, isToyota}) {
-  let car;
-  if (isHonda) {
-    car = <h2>Honda!!</h2>;
-  }
-  if (isToyota) {
-    car = <h2>Toyota!!</h2>;
-  }
-  if (!isHonda && !isToyota) {
-    return null;
-  }
-  return <>
-    <h1>This is brand : <span style={{color: 'grey'}}>{car ? car : "No name."}</span></h1> 
-    {car && <h4>Inline logical</h4>}
-  </>
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    {/* <HelloWorldComp /> */}
-    <Car isHonda={true} />
-    <Car isToyota={true} />
-    <Car />
+    <Quiz />
   </React.StrictMode>,
   document.getElementById('root')
 );
