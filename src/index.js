d6f48046d9d1cae3453fdbd9db561c6f07bdf665
builder <builder@odds.team>
2020-08-15 14:51:45 +0700

    Toggle bind scope

--- src/index.js
+++ src/index.js
@@ -4,99 +4,29 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-class ItemEvent extends React.Component {
+class Toggle extends React.Component {
 
   state = {
-    numbers: [1, 2, 3, 4, 5]
+    isToggleOn: false
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      const state = this.state.numbers;
-      state.shift();
-      this.setState({ numbers: [...state]});
-    })
-  }
-
-  add = () => {
-    this.setState({ numbers: [...this.state.numbers, this.state.numbers[this.state.numbers.length-1] + 1]})
+  handleClick() {
+    this.setState((p) => ({isToggleOn: !p.isToggleOn}))
   }
 
   render() {
-    const { numbers } = this.state;
+    const { isToggleOn } = this.state;
     return <>
-      <button onClick={this.add}>Add</button>
-      <ItemList numbers = {numbers}/>
-    </>
-  }
-}
-
-function ItemList({numbers}) {
-  return<>
-    <ul>
-      {numbers.map(n => <li key={n}>{n}</li>)}
-    </ul>
-  </>
-}
-
-class NewItemEventQuiz extends React.Component {
-
-  state = {
-    listText: [],
-    text: ""
-  }
-
-  add = () => {
-    this.setState({ listText: [...this.state.listText, this.state.text]})
-  }
-
-  onChange(event) {
-    this.setState({ text: event.target.value});
-  }
-
-  render() {
-    const { listText } = this.state;
-    return <>
-      <h5>Quiz Two</h5>
-      <input onChange={(event) => this.onChange(event)} />
-      <button onClick={this.add}>Add</button>
-      <ItemList numbers = {listText}/>
-    </>
-  }
-}
-
-class ExampleTwo extends React.Component {
-  state = {
-    items: []
-  }
-
-  add = (event) => {
-    if(event.key === 'Enter') { // directly access to DOM
-      const { value } = event.target;
-      this.setState((previousState) => (
-        {items: [...previousState.items, value]}
-      ));
-      event.target.value = '';
-    }
-  }
-
-  render() {
-    const { items } = this.state;
-    return <>
-      <h5>Example Two</h5>
-      <input onKeyUp={this.add} />
-      <ul>
-        {items.map((v) => <li key={v}>{Number.isNaN(v) ? 'Number' : 'No Number'}</li>)}
-      </ul>
+      <button onClick={this.handleClick.bind(this)}>
+        {isToggleOn ? 'On' : 'Off'}
+      </button>
     </>
   }
 }
 
 ReactDOM.render(
   <React.StrictMode>
-    <ItemEvent />
-    <NewItemEventQuiz />
-    <ExampleTwo />
+    <Toggle/>
   </React.StrictMode>,
   document.getElementById('root')
 );
